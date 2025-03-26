1d3fb814899e1e272963555935afd7f6832fad44
Shahdad Neda <shahdad.neda@gmail.com>
2025-03-26 00:52:00 -0400

    Initial Commit

---
+++ AquaTicker/AquaTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaTickerApp.swift
+//  AquaTicker
+//
+//  Created by Shahdad Neda on 2025-03-26.
+//
+
+import SwiftUI
+
+@main
+struct AquaTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AquaTickerTests/AquaTickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AquaTickerTests.swift
+//  AquaTickerTests
+//
+//  Created by Shahdad Neda on 2025-03-26.
+//
+
+import Testing
+@testable import AquaTicker
+
+struct AquaTickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
